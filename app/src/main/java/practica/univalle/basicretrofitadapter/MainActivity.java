20f3178da63c671c733e0b5f40b0ecccb6e520d4
adancondori <adancondoric@gmail.com>
2023-05-14 15:31:23 -0400

    retrofit example

--- app/src/main/java/practica/univalle/basicretrofitadapter/MainActivity.java
+++ app/src/main/java/practica/univalle/basicretrofitadapter/MainActivity.java
@@ -5,12 +5,18 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import practica.univalle.basicretrofitadapter.Adapters.UserAdapter;
 import practica.univalle.basicretrofitadapter.Models.User;
+import practica.univalle.basicretrofitadapter.Service.UserResponse;
+import practica.univalle.basicretrofitadapter.Service.UserService;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,16 +29,56 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initAdapter();
+        getAllUser();
     }
 
     public void initAdapter(){
         userList = new ArrayList<>();
-        initUsers();
         recyclerView = findViewById(R.id.recyclerView);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         userAdapter = new UserAdapter(this,userList);
         recyclerView.setAdapter(userAdapter);
     }
+
+    private void getAllUser() {
+        Call<UserResponse> call = UserService.getMovieAPI().getAllUser();
+        call.enqueue(new Callback<UserResponse>() {
+            @Override
+            public void onResponse(Call<UserResponse> call, Response<UserResponse> response) {
+                if (response.isSuccessful()) {
+                    userList.addAll(response.body().getUser());
+                    userAdapter.notifyDataSetChanged();
+                } else {
+                    Log.e("MainActivity", "Error al obtener la lista de películas: " + response.message());
+                }
+            }
+
+            @Override
+            public void onFailure(Call<UserResponse> call, Throwable t) {
+                Log.e("MainActivity", "Error al obtener la lista de películas: " + t.getMessage());
+            }
+        });
+    }
+    private void getUserPerPage() {
+        Call<UserResponse> call = UserService.getMovieAPI().getUserPerPage(1,12);
+        call.enqueue(new Callback<UserResponse>() {
+            @Override
+            public void onResponse(Call<UserResponse> call, Response<UserResponse> response) {
+                if (response.isSuccessful()) {
+                    userList.addAll(response.body().getUser());
+                    userAdapter.notifyDataSetChanged();
+                } else {
+                    Log.e("MainActivity", "Error al obtener la lista de películas: " + response.message());
+                }
+            }
+
+            @Override
+            public void onFailure(Call<UserResponse> call, Throwable t) {
+                Log.e("MainActivity", "Error al obtener la lista de películas: " + t.getMessage());
+            }
+        });
+    }
+/*
     public void initUsers(){
         userList.add(new User(1,"matatan@gmail.com","Andres","Pereira","https://reqres.in/img/faces/1-image.jpg"));
         userList.add(new User(2,"matatan1@gmail.com","Pero","Pereira","https://reqres.in/img/faces/3-image.jpg"));
@@ -43,7 +89,6 @@ public class MainActivity extends AppCompatActivity {
         userList.add(new User(7,"matatan6@gmail.com","Juan","Pereira","https://reqres.in/img/faces/4-image.jpg"));
         userList.add(new User(8,"matatan7@gmail.com","Jhon","Pereira","https://reqres.in/img/faces/4-image.jpg"));
         userList.add(new User(9,"matatan@gmail.com","Andres","Pereira","https://reqres.in/img/faces/4-image.jpg"));
-
-
     }
+ */
 }
