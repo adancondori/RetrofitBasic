ae97e116c31c9fa2b5f950a9703f2e92e62cb33a
adancondori <adancondoric@gmail.com>
2023-06-11 22:13:46 -0400

    initial shared preference

--- app/src/main/java/practica/univalle/basicretrofitadapter/MainActivity.java
+++ app/src/main/java/practica/univalle/basicretrofitadapter/MainActivity.java
@@ -3,12 +3,33 @@ package practica.univalle.basicretrofitadapter;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import practica.univalle.basicretrofitadapter.util.SharedPrefsUtil;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText userName;
+    private SharedPrefsUtil sharedPrefsUtil;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        sharedPrefsUtil = new SharedPrefsUtil(this);
+        userName = findViewById(R.id.editTextUserName);
     }
+
+
+    public void saveUserName(View view) {
+        sharedPrefsUtil.saveString("username", userName.getText().toString());
+        Toast.makeText(this, "Username saved!", Toast.LENGTH_SHORT).show();
+    }
+
+    public void retrieveUserName(View view) {
+        String name = sharedPrefsUtil.getString("username");
+        Toast.makeText(this, "Username: " + name, Toast.LENGTH_SHORT).show();
+    }
+
 }
