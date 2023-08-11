f8c7ffa37adc4a27dfd5ec7d72e1d4e2d811b583
adancondori <adancondoric@gmail.com>
2023-08-11 07:40:16 -0400

    horse race 2

--- app/src/main/java/practica/univalle/basicretrofitadapter/HorseRaceActivity.java
+++ app/src/main/java/practica/univalle/basicretrofitadapter/HorseRaceActivity.java
@@ -3,7 +3,9 @@ package practica.univalle.basicretrofitadapter;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.ProgressBar;
 
 import practica.univalle.basicretrofitadapter.models.Horse;
@@ -12,24 +14,28 @@ public class HorseRaceActivity extends AppCompatActivity {
 
     private static final int NUM_CABALLOS = 2;
 
-    private ProgressBar[] progressBars = new ProgressBar[NUM_CABALLOS];
+
+    private Horse[] horses = new Horse[NUM_CABALLOS];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_horse_race);
 
-        progressBars[0] = findViewById(R.id.horse1);
-        progressBars[1] = findViewById(R.id.horse2);
+        LinearLayout layout = findViewById(R.id.horsesLayout);
+        for (int i = 0; i < NUM_CABALLOS; i++) {
+            View horseView = getLayoutInflater().inflate(R.layout.horse_layout, layout, false);
+            horses[i] = new Horse(horseView);
+            layout.addView(horseView);
+        }
 
         Button startRaceButton = findViewById(R.id.startRaceButton);
         startRaceButton.setOnClickListener(v -> startRace());
     }
 
     private void startRace() {
-        for (int i = 0; i < NUM_CABALLOS; i++) {
-            new Thread(new Horse(progressBars[i])).start();
+        for (Horse horse : horses) {
+            new Thread(horse).start();
         }
     }
-
 }
--- app/src/main/java/practica/univalle/basicretrofitadapter/models/Horse.java
+++ app/src/main/java/practica/univalle/basicretrofitadapter/models/Horse.java
@@ -1,27 +1,40 @@
 package practica.univalle.basicretrofitadapter.models;
 
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.ProgressBar;
 
-public class Horse implements Runnable {
-    private final ProgressBar progressBar;
-    private static final int MAX_PROGRESS = 100;
+import practica.univalle.basicretrofitadapter.R;
 
-    public Horse(ProgressBar progressBar) {
-        this.progressBar = progressBar;
+public class Horse implements Runnable {
+    private final ImageView horseImage;
+    public LinearLayout horsesLayout;
+    private static final int MAX_DISTANCE = 1000;
+
+    public Horse(View view) {
+        horseImage = view.findViewById(R.id.horseImage);
+        horsesLayout = view.findViewById(R.id.racetrackLayout);
     }
 
     @Override
     public void run() {
-        int progress = 0;
-        while (progress < MAX_PROGRESS) {
+        int distance = 0;
+        while (distance < MAX_DISTANCE) {
             try {
-                Thread.sleep((long) (Math.random() * 200));
+                Thread.sleep((long) (Math.random() * 200)); // Simula el tiempo que tarda el caballo en avanzar
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
-            progress += (int) (Math.random() * 10);
-            int finalProgress = Math.min(progress, MAX_PROGRESS);
-            progressBar.post(() -> progressBar.setProgress(finalProgress));
+            distance += (int) (Math.random() * 10); // Simula la distancia que avanza el caballo
+            int finalDistance = Math.min(distance, MAX_DISTANCE);
+            float progress = (float) finalDistance / MAX_DISTANCE;
+            horseImage.post(() -> {
+                LinearLayout.LayoutParams params = (LinearLayout.LayoutParams) horseImage.getLayoutParams();
+                params.leftMargin = (int) (progress * (horsesLayout.getWidth() - horseImage.getWidth()));
+                horseImage.setLayoutParams(params);
+            }); // Actualiza la posición de la imagen
         }
     }
 }
