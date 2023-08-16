471d2f7a064d62dd8c1652ffdd2a28fbbb845500
adancondori <adancondoric@gmail.com>
2023-08-16 09:50:20 -0400

    example brodcast

--- app/src/main/java/practica/univalle/basicretrofitadapter/MainActivity.java
+++ app/src/main/java/practica/univalle/basicretrofitadapter/MainActivity.java
@@ -3,18 +3,23 @@ package practica.univalle.basicretrofitadapter;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    public static final String CUSTOM_BROADCAST_ACTION = "practica.univalle.basicretrofitadapter.CUSTOM_ACTION";
+    public static final String CUSTOM_BROADCAST_ACTION = "practica.univalle.basicretrofitadapter";
+    private MyReceiver myReceiver;
+    public TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button broadcastButton = findViewById(R.id.broadcast_button);
+        textView = findViewById(R.id.result_text);
         broadcastButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -22,5 +27,10 @@ public class MainActivity extends AppCompatActivity {
                 sendBroadcast(customBroadcastIntent);
             }
         });
+        if (myReceiver == null) {
+            myReceiver = new MyReceiver(textView);
+            IntentFilter intentFilter = new IntentFilter(CUSTOM_BROADCAST_ACTION);
+            registerReceiver(myReceiver, intentFilter);
+        }
     }
 }
--- app/src/main/java/practica/univalle/basicretrofitadapter/MyReceiver.java
+++ app/src/main/java/practica/univalle/basicretrofitadapter/MyReceiver.java
@@ -3,16 +3,22 @@ package practica.univalle.basicretrofitadapter;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.util.Log;
 import android.widget.TextView;
 
 public class MyReceiver  extends BroadcastReceiver {
-    private TextView textView;
+    public TextView textView;
+
+    public MyReceiver() {
+    }
 
     public MyReceiver(TextView textView) {
         this.textView = textView;
     }
+
     @Override
     public void onReceive(Context context, Intent intent) {
+        Log.d("TAG", "onReceive: ");
         if (intent.getAction() != null && intent.getAction().equals(MainActivity.CUSTOM_BROADCAST_ACTION)) {
             textView.setText("Broadcast recibido!");
         }
